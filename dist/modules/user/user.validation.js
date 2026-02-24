1b5353bcfa91fea956909bf5f3547eee9b63544f
nabila-momtarin <nabila.momtarin.me@gmail.com>
2026-02-24 11:30:43 +0600

    Remove dist from git tracking

--- dist/modules/user/user.validation.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// export class UserValidator {
-//     constructor() {}
-//     validationResult = async (req: Request, res: Response): Promise<any> => {
-//         const {email, password} = req.body;
-//     }
-// }
