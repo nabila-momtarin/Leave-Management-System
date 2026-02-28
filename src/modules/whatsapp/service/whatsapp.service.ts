71376bbb9ba3d4c4299edc977bdb5636e2951b94
nabila-momtarin <nabila.momtarin.me@gmail.com>
2026-02-28 16:09:48 +0600

    Update WhatsApp API version and change message type to template

--- src/modules/whatsapp/service/whatsapp.service.ts
+++ src/modules/whatsapp/service/whatsapp.service.ts
@@ -2,24 +2,31 @@ import axios from "axios";
 
 export class WhatsAppService {
   async sendMessage(phone: string, message: string) {
-    const url = `https://graph.facebook.com/v22.0/${process.env.PHONE_NUMBER_ID}/messages`;
-//v22.0
+    const url = `https://graph.facebook.com/v18.0/${process.env.PHONE_NUMBER_ID}/messages`;
+    //v22.0
     const response = await axios.post(
       url,
       {
         messaging_product: "whatsapp",
         to: phone,
-        type: "text",
-        text: {
-          body: message,
-        },
+        // type: "text",
+        // text: {
+        //   body: message,
+        // },
+        type: "template",
+        template: {
+          name: "hello_world",
+          language: {
+            code: "en_US",
+          }
+        }
       },
       {
         headers: {
           Authorization: `Bearer ${process.env.WHATSAPP_ACCESS_TOKEN}`,
           "Content-Type": "application/json",
         },
-      }
+      },
     );
 
     console.log("META RESPONSE:", JSON.stringify(response.data, null, 2));
